878328cd3f9da9989f022c14c39acd3a8ea94b06
cecemel <ruiz.felixruiz@gmail.com>
2017-09-11 13:58:27 +0200

    first version -> server starts (unittests not, since no tests-private) next commit

---
+++ README.md
@@ -0,0 +1,73 @@
+# premieaanvragen-dev
+
+## deployment of dev environment for the 'premieaanvragen' app
+
+### build instructions
+
+#### assumes
+- access to OE private repos
+- osx/linux environment
+- docker installed
+- python installed
+- git
+- your wildcards-private.ini files
+- pycharm enterprise edition
+
+#### general setup
+```
+# assumes you have access to OE private repos
+git clone --recursive https://github.com/cecemel/premieaanvragen-dev.git
+cd premieaanvragen-dev
+# now, you have to add the development-private.ini and wildcards files on the right place,
+ask your colleagues for help if you don't know
+```
+
+#### building the frontend
+```
+TODO :-/
+```
+
+### building, migrating & init elastic, dummy data etc...
+```
+# assumes you are in folder premieaanvragen-dev
+docker-compose stop; docker-compose rm -f; #not required, but cleans your working environment
+# assumes you are in premieaanvragen-dev
+python build_images.py [GITHUB_USER] [GITHUB_PASS];
+python migrate_dbs.py;
+python init_data.py;
+```
+
+#### reset backend one liner
+```
+docker-compose stop; docker-compose rm -f; rm -rf data/*; python build_images.py [GITHUB_USER] [GITHUB_PASS]; python migrate_dbs.py; python init_data.py;
+```
+
+### running in pycharm
+see e.g.
+https://blog.jetbrains.com/pycharm/2017/03/docker-compose-getting-flask-up-and-running/
+
+### rebuilding and running a dependent service
+```
+# e.g.
+python build_images.py [GITHUB_USER] [GITHUB_PASS] storageprovider
+```
+
+### some git submodule tricks
+- tutorial: https://git-scm.com/book/en/v2/Git-Tools-Submodules
+- some good submodule SO: https://stackoverflow.com/questions/1030169/easy-way-to-pull-latest-of-all-git-submodules
+- bringing *-dev repo up to data: git pull; git submodule update --init --recursive
+
+### caveats-todos
+- on slow networks, you'll might have to build a couple of times (2,3) again, because some scripts are not robust. needs fix
+- if you modify the production.ini, you will have to build the image again
+- docs need to be build automatically
+- archive_feed is not build currently
+- no documentgenerator
+- currently, only dummy data is imported, if you want data from the db.dump -> you will need to fiddle a little with the docker files...
+- if you get error HTTPError similar to: "401 Client Error: Unauthorized for url": restart your docker daemon
+- scripts contain some hard coded parameters and should be cleaned
+- currently, if you change code or config in dependent services, you will have to build and run every time again
+- if you add a new dependent service and it needs a database, you will have to remove the postgres folder, and start the build from scratch
+- working with private pypi server is still a hack, needs a fix
+- a generic base image should be extract to speed up image build
+- clean-up scripts, docker-compose should be sufficient for all migrations
---
+++ init_data.py
@@ -0,0 +1,162 @@
+import os, shutil, time
+import migrate_dbs
+
+ELASTIC_INITS_FOLDER = "data-inits"
+DOCKER_FILE = "Dockerfile-data-init"
+DOCKER_REPO = "premieaanvragen-dev"
+ELASTIC_IMAGE = "geosolutions/elasticsearch-plugins"
+ELASTIC_CONTAINER_NAME = "premieaanvragen-elastic-init"
+ELASTIC_DATA = "$(pwd)/data/elastic"
+STORAGE_PROVIDER_IMAGE = "premieaanvragen-dev/storageprovider:latest"
+STORAGE_PROVIDER_CONTAINER_NAME = "storageprovider-init"
+STORAGE_PROVIDER_DATA = "$(pwd)/data/storageprovider"
+STORAGE_PROVIDER_DATA_MAP = "{}:/premieaanvragen_store".format(STORAGE_PROVIDER_DATA)
+REDIS_IMAGE = "redis"
+REDIS_CONTAINER_NAME = "premieaanvragen-redis-init"
+
+
+def start_redis():
+    try:
+        stop_and_clean_redis_container()
+    except:
+        print("issue cleaning docker images, let's proceed and see...")
+
+    _exec_command("docker run -p '6379:6379' --name {} {} &".format(REDIS_CONTAINER_NAME, REDIS_IMAGE))
+
+    print("wait for redis to boot (10 secs)")
+    time.sleep(10)
+    _exec_command("docker run "
+                  "--link {}:redis "
+                  "premieaanvragen-dev/redis-checker:latest"
+                  .format(REDIS_CONTAINER_NAME))
+
+
+def start_storage_provider():
+    try:
+        stop_and_clean_storage_provider_container()
+    except:
+        print("issue cleaning docker images, let's proceed and see...")
+
+    _exec_command("mkdir -p {}".format(STORAGE_PROVIDER_DATA))
+    _exec_command("docker run -p '6544:6544' --name {} -v {} {}&"
+                  .format(STORAGE_PROVIDER_CONTAINER_NAME, STORAGE_PROVIDER_DATA_MAP, STORAGE_PROVIDER_IMAGE))
+
+    print("wait for storage provider to boot (10 secs)")
+    time.sleep(10)
+    _exec_command("docker run "
+                  "--link {}:storageprovider "
+                  "premieaanvragen-dev/storageprovider-checker:latest"
+                  .format(STORAGE_PROVIDER_CONTAINER_NAME))
+
+
+def start_elastic():
+    # make sure it starts clean
+    try:
+        stop_and_clean_elastic_container()
+    except:
+        print("issue cleaning docker images, let's proceed and see...")
+
+    _exec_command("docker run -p '9200:9200' --name {} -v {}:/usr/share/elasticsearch/data {} &".format(ELASTIC_CONTAINER_NAME,
+                                                                                ELASTIC_DATA,
+                                                                                ELASTIC_IMAGE))
+
+    print("wait for elastic to boot (10 secs)")
+    time.sleep(10)
+    _exec_command("docker run "
+                  "--link {}:elastic "
+                  "premieaanvragen-dev/elastic-checker:latest"
+                  .format(ELASTIC_CONTAINER_NAME))
+
+
+def run_elastic_init():
+    try:
+        start_storage_provider()
+        start_elastic()
+        start_redis()
+        migrate_dbs.start_db()
+
+        print('elastic ready, moving on...')
+
+        current_path = os.path.dirname(os.path.realpath(__file__))
+        migrations_dir = os.path.join(current_path, ELASTIC_INITS_FOLDER)
+
+        assert os.path.isdir(migrations_dir), "Expected a data-inits dir..."
+
+        migrations_folders = _listdir_not_hidden(migrations_dir)
+
+        for folder in migrations_folders:
+            target_folder = os.path.join(current_path, folder)
+
+            docker_file = os.path.join(current_path, ELASTIC_INITS_FOLDER, folder, DOCKER_FILE)
+
+            print("copy docker file ")
+            shutil.copy(docker_file, target_folder)
+
+            print("changing dir to {}".format(target_folder))
+            os.chdir(target_folder)
+
+            # do the docker build
+            try:
+                print("starting initialization")
+                print("building image")
+                docker_image_repo = "{}/{}-migration:latest".format(DOCKER_REPO, folder)
+                _exec_command("docker build -f {} -t {} .".format(DOCKER_FILE, docker_image_repo))
+
+                print("fire container and run migration")
+                run_command = "docker run " \
+                              "--link {}:elastic " \
+                              "--link {}:postgres  " \
+                              "--link {}:storageprovider " \
+                              "--link {}:redis "\
+                    .format(ELASTIC_CONTAINER_NAME,
+                            migrate_dbs.DATABASE_CONTAINER_NAME,
+                            STORAGE_PROVIDER_CONTAINER_NAME,
+                            REDIS_CONTAINER_NAME)
+                run_command += "{}".format(docker_image_repo)
+
+                print("firing!")
+                print(run_command)
+
+                _exec_command(run_command)
+
+            finally:
+                # clean up
+                print("changing dir back to {}".format(current_path))
+                os.remove(os.path.join(target_folder, DOCKER_FILE))
+
+                os.chdir(current_path)
+
+    finally:
+        stop_and_clean_redis_container()
+        stop_and_clean_elastic_container()
+        stop_and_clean_storage_provider_container()
+        migrate_dbs.stop_and_clean_db_container()
+
+
+def stop_and_clean_storage_provider_container():
+    _exec_command("docker stop {}; docker rm {}".format(STORAGE_PROVIDER_CONTAINER_NAME,
+                                                        STORAGE_PROVIDER_CONTAINER_NAME))
+
+
+def stop_and_clean_elastic_container():
+    _exec_command("docker stop {}; docker rm {}".format(ELASTIC_CONTAINER_NAME, ELASTIC_CONTAINER_NAME))
+
+
+def stop_and_clean_redis_container():
+    _exec_command("docker stop {}; docker rm {}".format(REDIS_CONTAINER_NAME, REDIS_CONTAINER_NAME))
+
+
+def _exec_command(command):
+    result = os.system(command)
+    if not result == 0:
+        raise Exception("Error while executing command {}".format(command))
+
+
+def _listdir_not_hidden(path):
+    for f in os.listdir(path):
+        if not f.startswith('.'):
+            yield f
+
+
+if __name__ == '__main__':
+    run_elastic_init()
